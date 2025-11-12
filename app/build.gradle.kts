55a41a2bb133ee88642b6451f2ff7ec0602a0486
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2025-11-12 16:16:22 +0530

    added dependencies
    Dependencies such as Hilt, navigation, adaptive are added for development.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // For Hilt
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    // Serialization
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
 }
 
 android {
@@ -25,7 +31,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -50,6 +56,19 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // For hilt
+    implementation("com.google.dagger:hilt-android:2.57.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.57.1")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.9.6")
+
+    // Adaptive window
+    implementation("androidx.compose.material3.adaptive:adaptive:1.3.0-alpha03")
+    implementation("androidx.compose.material3.adaptive:adaptive-layout:1.3.0-alpha03")
+    implementation("androidx.compose.material3.adaptive:adaptive-navigation:1.3.0-alpha03")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    // For Hilt
+    id("com.google.dagger.hilt.android") version "2.57.1" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+
+    // Serialization
+    kotlin("plugin.serialization") version "2.0.0" apply false
 }
